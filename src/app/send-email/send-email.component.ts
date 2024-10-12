6ef3857cb72e9fb53b5139c3fedd4478273935fa
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-11 21:54:00 -0300

    map iteration

--- src/app/send-email/send-email.component.ts
+++ src/app/send-email/send-email.component.ts
@@ -1,20 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Form, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-send-email',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './send-email.component.html',
   styleUrl: './send-email.component.css'
 })
 export class SendEmailComponent {
   //variables : Map<String, String> = new Map<String, String>
-  emailToSend : string | undefined
-  subject : string | undefined
-  variables : Map<string, string> | undefined
+  emailToSend : string = ""
+  subject : string = ""
+  name : string = ""
+  value : string = ""
+  variables : Map<string, string> = new Map<string, string>()
 
-  enviar(form : Form){
+  enviar(form : Form) {
 
+  }
+  addVariables() {
+    if (this.name != null && this.name !== "" && this.value != null && this.value !== "") {
+      this.variables.set(this.name, this.value);
+      this.name = "";
+      this.value = "";
+    }    
   }
 }
