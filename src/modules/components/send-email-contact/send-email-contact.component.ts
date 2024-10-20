2fc55c3758a9f8ab2332e067e866419ad843721b
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-20 19:27:11 -0300

    componente nuevo

---
+++ src/modules/components/send-email-contact/send-email-contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-send-email-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './send-email-contact.component.html',
+  styleUrl: './send-email-contact.component.css'
+})
+export class SendEmailContactComponent {
+
+}
