fa7295d175ee56562e4ce2a361a7c48dd34000d6
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-20 20:25:15 -0300

    formulario comenzado

--- src/modules/components/send-email-contact/send-email-contact.component.ts
+++ src/modules/components/send-email-contact/send-email-contact.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { EmailServiceService } from '../../../app/services/email-service.service';
 
 @Component({
   selector: 'app-send-email-contact',
@@ -9,5 +11,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './send-email-contact.component.css'
 })
 export class SendEmailContactComponent {
+submit() {
+throw new Error('Method not implemented.');
+}
+  emailDataWithContact = new FormGroup({
+    subject : new FormControl(''),
+    variables : new FormControl(''),
+    template_id : new FormControl(''),
+    contact_id : new FormControl('')
+  })
 
+  service : EmailServiceService = inject(EmailServiceService)
 }
