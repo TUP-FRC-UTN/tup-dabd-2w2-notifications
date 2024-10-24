c77b23d059beb75349b96e383fd21c99a3effad8
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-24 16:04:46 -0300

    form y clase

--- src/modules/components/contact-modify-subs-email/contact-modify-subs-email.component.ts
+++ src/modules/components/contact-modify-subs-email/contact-modify-subs-email.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ContactService } from '../../../app/services/contact.service';
+import { Contact } from '../../../app/models/contact';
 
 @Component({
   selector: 'app-contact-modify-subs-email',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './contact-modify-subs-email.component.html',
   styleUrl: './contact-modify-subs-email.component.css'
 })
-export class ContactModifySubsEmailComponent {
+@Inject('ContactService')
+export class ContactModifySubsEmailComponent implements OnInit{
+  
+  serviceContact : ContactService = new ContactService()
 
+  modification : FormGroup = new FormGroup({
+    contactId : new FormControl('', Validators.required),
+    subscriptionId : new FormControl('', Validators.required),
+    subscriptionValue : new FormControl(false)
+  })
+
+  allContacts : Contact[] = []
+  selectedContactId : number = 0
+  selectedContact : Contact | undefined
+
+  ngOnInit(): void {
+    this.serviceContact.getAllContacts().subscribe((data) => {
+      data.forEach(c => {
+        if (c.contactType === 'EMAIL') {
+          this.allContacts.push(c)
+        }
+      })
+    })
+    this.serviceContact.getContactById(this.selectedContactId).subscribe((data) => {
+      this.selectedContact = data
+    })
+  }
+
+  submit() {
+
+  }
 }
