9c624de9ad4a2a89f0689b5968264aa5ccdc46d8
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-24 16:48:46 -0300

    servicio y subs

---
+++ src/app/models/subscription.ts
@@ -0,0 +1,5 @@
+export interface Subscription {
+    id : number
+    name : string
+    isUnsubscribable : Boolean
+}
--- src/modules/components/contact-modify-subs-email/contact-modify-subs-email.component.ts
+++ src/modules/components/contact-modify-subs-email/contact-modify-subs-email.component.ts
@@ -3,6 +3,9 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ContactService } from '../../../app/services/contact.service';
 import { Contact } from '../../../app/models/contact';
+import { SubscriptionService } from '../../../app/services/subscription.service';
+import { Subscription } from '../../../app/models/subscription';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contact-modify-subs-email',
@@ -12,9 +15,11 @@ import { Contact } from '../../../app/models/contact';
   styleUrl: './contact-modify-subs-email.component.css'
 })
 @Inject('ContactService')
+@Inject('SubscriptionService')
 export class ContactModifySubsEmailComponent implements OnInit{
   
   serviceContact : ContactService = new ContactService()
+  serviceSubs : SubscriptionService = new SubscriptionService()
 
   modification : FormGroup = new FormGroup({
     contactId : new FormControl('', Validators.required),
@@ -23,8 +28,9 @@ export class ContactModifySubsEmailComponent implements OnInit{
   })
 
   allContacts : Contact[] = []
-  selectedContactId : number = 0
+  //selectedContactId : number = 0
   selectedContact : Contact | undefined
+  allSubsFromSelectedContact : Subscription[] = []
 
   ngOnInit(): void {
     this.serviceContact.getAllContacts().subscribe((data) => {
@@ -34,10 +40,33 @@ export class ContactModifySubsEmailComponent implements OnInit{
         }
       })
     })
-    this.serviceContact.getContactById(this.selectedContactId).subscribe((data) => {
+    this.serviceContact.getContactById(this.modification.get('contactId')?.value).subscribe((data) => {
       this.selectedContact = data
     })
   }
+  
+  //Al array de nombres lo manda como array de objetos Subscription que coincidan con ese name
+  setSubscriptions(): Observable<Subscription[]> {
+    return new Observable((observer) => {
+      this.serviceSubs.getAllSubsriptions().subscribe((data) => {
+        const subscriptionsMap = new Set(data.map(sub => sub.name));
+        const aux = this.selectedContact?.subscriptions
+          .filter(sub1 => subscriptionsMap.has(sub1))
+          .map(sub1 => data.find(sub2 => sub2.name === sub1))
+          .filter((sub): sub is Subscription => sub !== undefined); 
+  
+        observer.next(aux);
+        observer.complete();
+      });
+    });
+  }
+  loadSubscriptions() {
+    this.setSubscriptions().subscribe((subscriptions) => {
+      this.allSubsFromSelectedContact = subscriptions;
+    });
+  }
+  
+  
 
   submit() {
 
