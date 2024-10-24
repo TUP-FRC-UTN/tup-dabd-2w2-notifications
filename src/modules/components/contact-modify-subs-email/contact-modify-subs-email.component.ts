46b7393122c4f734f9d873512111a6ed038b4787
Val <404931@tecnicatura.frc.utn.edu.ar>
2024-10-24 15:35:36 -0300

    componente nuevo

---
+++ src/modules/components/contact-modify-subs-email/contact-modify-subs-email.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-modify-subs-email',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-modify-subs-email.component.html',
+  styleUrl: './contact-modify-subs-email.component.css'
+})
+export class ContactModifySubsEmailComponent {
+
+}
